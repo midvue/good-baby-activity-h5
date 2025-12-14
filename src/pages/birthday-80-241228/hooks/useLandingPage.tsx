5f46c2331fd3158140d60eb450b344979778ab2c
Spig <510034906@qq.com>
2025-12-14 22:21:35 +0800

    feat: 添加80大寿的活动

--- src/pages/birthday-80-241228/hooks/useLandingPage.tsx
+++ src/pages/birthday-80-241228/hooks/useLandingPage.tsx
@@ -5,6 +5,10 @@ export let useLandingPage = (onClickAccept: Function) => {
     render: () => (
       <div class='birthday-landing-page' onClick={() => onClickAccept()}>
         <img
+          class='landing-img img-title-01'
+          src={$CDN_BASE_URL + 'birthday-80-241228/img_title.png'}
+        />
+        <img
           class='landing-img img-cloud-left-01'
           src={$CDN_BASE_URL + 'birthday-80-241228/img_cloud_left.png'}
         />
@@ -18,6 +22,10 @@ export let useLandingPage = (onClickAccept: Function) => {
           onLoad={hideHtmlLoading}
         />
         <img src={imgBtnAccept} alt='' class='birthday-btn' />
+        <img
+          class='landing-img img-time-address'
+          src={$CDN_BASE_URL + 'birthday-80-241228/img_time_address.png'}
+        />
       </div>
     )
   }
