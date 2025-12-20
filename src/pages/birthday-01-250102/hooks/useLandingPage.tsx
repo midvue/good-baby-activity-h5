6838f369479938ff643928d088edfb9ee8090f2c
Spig <510034906@qq.com>
2025-12-20 17:06:28 +0800

    feat: 添加周岁分享

--- src/pages/birthday-01-250102/hooks/useLandingPage.tsx
+++ src/pages/birthday-01-250102/hooks/useLandingPage.tsx
@@ -17,6 +17,10 @@ export let useLandingPage = (onClickAccept: Function) => {
           src={$CDN_BASE_URL + 'birthday-01-250102/img_fireworks_01.png'}
         />
         <img
+          class='landing-img img-fireworks-03'
+          src={$CDN_BASE_URL + 'birthday-01-250102/img_fireworks_01.png'}
+        />
+        <img
           class='landing-img img-cloud-right-01'
           src={$CDN_BASE_URL + 'birthday-01-250102/img_cloud_right.png'}
         />
@@ -26,6 +30,11 @@ export let useLandingPage = (onClickAccept: Function) => {
           onLoad={hideHtmlLoading}
         />
         <img src={imgBtnAccept} alt='' class='birthday-btn' />
+
+        <div class='landing-address-time'>
+          <div class='landing-address'>2026年01月02日 中午12点(公历)</div>
+          <div class='landing-time'>皂泥村 朱明聪家</div>
+        </div>
       </div>
     )
   }
